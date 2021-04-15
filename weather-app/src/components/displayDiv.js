e1bf6384a14608465c53ca5522a299e89d43f4bb
Keshav Kaushik <kkaushik070@gmail.com>
2021-04-15 17:36:40 +0530

    ""

--- weather-app/src/components/displayDiv.js
+++ weather-app/src/components/displayDiv.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Display = (props) => {
+
     return (<div style={{ textAlign:"center" }}>
-        <h1>
-            {props.message}
-        </h1>
+        <h1>{props.season}</h1> <br/>
+        <h1>{props.message}</h1>
     </div>)
 }
 
--- weather-app/src/components/seasons.js
+++ weather-app/src/components/seasons.js
@@ -6,10 +6,10 @@ const Season = (props) => {
     const month = new Date().getMonth() + 1
 
     if (props.lat > 0) {
-        return ((month > 3 && month < 9) ? <Display message='summer' /> : <Display message='winter' />)
+        return ((month > 3 && month < 9) ? <Display season='summer' message="Let's hit the Beach." /> : <Display season='winter' message="Damn it's chilly out there." />)
     }
     else {
-        return ((month > 3 && month < 9) ? <Display message='winter' /> : <Display message='summer' />)
+        return ((month > 3 && month < 9) ? <Display season='winter' message="Damn it's chilly out there." /> : <Display season='summer' message="Let's hit the Beach." />)
     }
 
 }
